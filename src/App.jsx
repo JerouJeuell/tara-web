cc686304975f826ddb554a5638b7bdfbbe3a6315
Chippy <jeroujeuellespelita@gmail.com>
2026-02-22 11:44:33 +0800

    Initial laravel project set up

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-rose-400 text-white text-2xl p-8">
+      Tara is working
+    </div>
+  )
+}
+
+export default App
